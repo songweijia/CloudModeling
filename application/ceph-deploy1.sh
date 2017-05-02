af67ada963a16fb51cc366dfae9e8761fdababe8
Sacheth Hegde <sacheg3@gmail.com>
2017-05-02 13:34:33 -0400

    Updated/fixed ceph deploy scripts

--- application/ceph-deploy1.sh
+++ application/ceph-deploy1.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# Argument 1: monitor node name
-MONITOR_NODE=$1
+# Argument 1: Admin node name
+ADMIN_NODE=$1
 
-# Argument 2: Private IP Subnet
-SUBNET=$2
+# Argument 2: Monitor node name
+MONITOR_NODE=$2
+
+# Argument 3: Private IP Subnet
+SUBNET=$3
+
+# Argument 4-n: All of the OSD node names
+OSD_NODES=${@:4}
+
+# OSD Pool Default Size:
+POOL_SIZE=2
+
+# echo "Summary:\n\nAdmin: $ADMIN_NODE\nMonitor: $MONITOR_NODE\nSubnet: $SUBNET\nOSD Node List: $OSD_NODES\n\n"
 
 cd ~
 mkdir cluster
@@ -14,13 +25,12 @@ ceph-deploy new $MONITOR_NODE
 
 # Modify 'ceph.conf' file with updated properties
 # Need these specifically for Fractus instances
-echo "osd pool default size = 2" >> ceph.conf
+echo "osd pool default size = $POOL_SIZE" >> ceph.conf
 echo "public network = $SUBNET" >> ceph.conf
 echo "[osd]" >> ceph.conf
 echo "filestore xattr use omap = true" >> ceph.conf
 
-# TODO: Make this parametrized
 ceph-deploy install ceph-osd1 ceph-osd2 ceph-osd3 ceph-mon1 ceph-admin
+ceph-deploy install $OSD_NODES $MONITOR_NODE $ADMIN_NODE 
 
 ceph-deploy mon create-initial
-
--- application/ceph-deploy2.sh
+++ application/ceph-deploy2.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
-#TODO: Parameterize further
-ceph-deploy osd prepare ceph-osd1:/var/local/osd1 ceph-osd2:/var/local/osd2
+# Note: Please look at the README and follow the instructions. There are a few things that need to be done
+# manually after running ceph-deploy1.sh and before running ceph-deploy2.sh (this script).
 
-ceph-deploy osd activate ceph-osd1:/var/local/osd1 ceph-osd2:/var/local/osd2
+# Argument 1: Admin node name
+ADMIN_NODE=$1
 
-ceph-deploy admin ceph-admin ceph-mon1 ceph-osd1 ceph-osd2 ceph-osd3
+# Argument 2: Monitor node name
+MONITOR_NODE=$2
+
+# Argument 4-n: All of the OSD node names
+OSD_NODES=${@:3}
+
+NUM_OSD=$(($# - 2))
+
+for i in `seq 1 $NUM_OSD`;
+do
+    # TODO: This is not parametrized, it assumed the OSD names are ceph-osd#
+    ceph deploy osd prepare ceph-osd"$i":/var/local/osd"$i"
+done
+
+for i in `seq 1 $NUM_OSD`;
+do
+    # TODO: This is not parametrized, it assumed the OSD names are ceph-osd#
+    ceph deploy osd activate ceph-osd"$i":/var/local/osd"$i"
+done
+
+ceph-deploy admin $ADMIN_NODE $MONITOR_NODE $OSD_NODES
 
 sudo chmod +r /etc/ceph/ceph.client.admin.keyring
-
