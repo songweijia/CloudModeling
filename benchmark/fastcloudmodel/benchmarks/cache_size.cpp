d53000fe43ee0cffdea8f23b2edca7e2c40fdaee
Weijia <ws393@cornell.edu>
2018-05-10 14:51:20 -0400

    bugfix: improved binary search with unbounded upper.

--- benchmark/fastcloudmodel/benchmarks/cache_size.cpp
+++ benchmark/fastcloudmodel/benchmarks/cache_size.cpp
@@ -5,39 +5,42 @@
 
 #define NUM_ITER_PER_DP         (5)
 #define BUFFER_ALIGNMENT        (4096)
+#define MEM_ALLOCATION		(256ull << 20)
 
 static int32_t binary_search (
   const uint32_t seed_kb,
   const double target_thp,
   const bool is_write,
   const int32_t search_depth,
+  void * workspace, // a memory workspace with MEM_ALLOCATION 256MB.
   uint32_t *output) {
 
-
   uint32_t lb = 0ul; // cache size lower bound in KB
-  uint32_t ub = seed_kb << 2; // cache size upperbound in KB, 4 times of seed_kb.
+  uint32_t ub = 0ul; // cache size upperbound in KB
   uint32_t pivot = seed_kb;
   double thps[NUM_ITER_PER_DP];
-  void *buf;
-
-  int32_t ret = posix_memalign(&buf, BUFFER_ALIGNMENT, ((size_t)ub)<<10);
-  RETURN_ON_ERROR(ret,"posix_memalign()");
+  int ret = 0;
 
   int loop = search_depth;
 
   while(loop --) {
     size_t buffer_size = ((size_t)pivot)<<10;
-    ret = sequential_throughput(buf,buffer_size,NUM_ITER_PER_DP,thps,is_write);
+    ret = sequential_throughput(workspace,buffer_size,NUM_ITER_PER_DP,thps,is_write);
     RETURN_ON_ERROR(ret,"sequential_throughput");
     double v = average(NUM_ITER_PER_DP,thps);
-    if (v < target_thp)
+    uint32_t new_pivot = pivot;
+    if (v < target_thp) { // go smaller
       ub = pivot;
-    else
+      new_pivot = (ub + lb) / 2;
+    } else { // go bigger
       lb = pivot;
-    if (pivot == (ub + lb) / 2) // no change
-      break;
-    else
-      pivot = (ub + lb) / 2;
+      if (ub == 0)
+        new_pivot = pivot << 1;
+      else
+        new_pivot = (ub + lb) / 2;
+    }
+    if (pivot == new_pivot) break;
+    else pivot = new_pivot;
   }
 
   *output = pivot;
@@ -55,14 +58,19 @@ int eval_cache_size(
   const int32_t search_depth) {
 
   int i,ret;
+  void *ws;
 
   boost_cpu();
+  ret = posix_memalign(&ws, BUFFER_ALIGNMENT, MEM_ALLOCATION);
+  RETURN_ON_ERROR(ret,"posix_memalign()");
 
   for(i=0;i<num_samples;i++) {
     double thp = lower_thp_GBps + (upper_thp_GBps - lower_thp_GBps) * (i + 1) / (num_samples + 1);
-    ret = binary_search (cache_size_hint_KB, thp, is_write, search_depth, &css[i]);
+    ret = binary_search (cache_size_hint_KB, thp, is_write, search_depth, ws, &css[i]);
     RETURN_ON_ERROR(ret,"binary_search");
   }
+
+  free (ws);
 
   return ret;
 }
