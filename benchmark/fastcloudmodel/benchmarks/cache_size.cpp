324908e0c319499fcb733c0aa2fcd8da0cc9e4f3
Weijia <ws393@cornell.edu>
2018-05-12 18:04:38 -0400

    improved binarysearch

--- benchmark/fastcloudmodel/benchmarks/cache_size.cpp
+++ benchmark/fastcloudmodel/benchmarks/cache_size.cpp
@@ -9,17 +9,29 @@
 
 static int32_t binary_search (
   const uint32_t seed_kb,
-  const double target_thp,
+  const double * target_thps,
+  const int num_samples,
   const bool is_write,
   const int32_t search_depth,
   void * workspace, // a memory workspace with MEM_ALLOCATION 256MB.
-  uint32_t *output) {
+  uint32_t *output,
+  const uint32_t LB = 0ul,
+  const uint32_t UB = 0ul) {
 
-  uint32_t lb = 0ul; // cache size lower bound in KB
-  uint32_t ub = 0ul; // cache size upperbound in KB
+  uint32_t lb = LB;
+  uint32_t ub = UB;
+
+  // done
+  if (num_samples == 0)
+    return 0;
+
+  // get the middle target
+  const double target_thp = target_thps[num_samples/2];
+
+  // search ...
   uint32_t pivot = seed_kb;
   double thps[NUM_ITER_PER_DP];
-  int ret = 0;
+  uint32_t ret = 0;
 
   int loop = search_depth;
 
@@ -43,7 +55,27 @@ static int32_t binary_search (
     else pivot = new_pivot;
   }
 
-  *output = pivot;
+  output[num_samples/2] = pivot;
+
+  // search others
+  uint32_t npivot = (lb + pivot)/2;
+  uint32_t nofst = 0;
+  uint32_t nlen = num_samples/2;
+  uint32_t nlb = LB;
+  uint32_t nub = pivot;
+  ret = binary_search(npivot,
+    target_thps + nofst,nlen,is_write,
+    search_depth-1,workspace,output+nofst,nlb,nub);
+  RETURN_ON_ERROR(ret,"binary_search, upper half");
+  npivot = UB?(pivot+UB)/2:2*pivot;
+  nofst = num_samples/2+1;
+  nlen = num_samples-num_samples/2-1;
+  nlb = pivot;
+  nub = ub;
+  ret = binary_search(npivot,
+    target_thps + nofst,nlen,is_write,
+    search_depth-1,workspace,output+nofst,nlb,nub);
+  RETURN_ON_ERROR(ret,"binary_search, lower half");
   
   return ret;
 }
@@ -64,11 +96,13 @@ int eval_cache_size(
   ret = posix_memalign(&ws, BUFFER_ALIGNMENT, MEM_ALLOCATION);
   RETURN_ON_ERROR(ret,"posix_memalign()");
 
-  for(i=0;i<num_samples;i++) {
-    double thp = lower_thp_GBps + (upper_thp_GBps - lower_thp_GBps) * (i + 1) / (num_samples + 1);
-    ret = binary_search (cache_size_hint_KB, thp, is_write, search_depth, ws, &css[i]);
-    RETURN_ON_ERROR(ret,"binary_search");
-  }
+  // printf("L3 Write Throughput: %.3f GB/s.\n", upper_thp_GBps);
+  // printf("Memory Write Throughput: %.3f GB/s.\n",lower_thp_GBps);
+  double thps[num_samples];
+  for(i=0;i<num_samples;i++)
+    thps[i] = upper_thp_GBps - (upper_thp_GBps - lower_thp_GBps) * (i + 1) / (num_samples + 1);
+  ret = binary_search (cache_size_hint_KB, thps, num_samples, is_write, search_depth, ws, css);
+  RETURN_ON_ERROR(ret,"binary_search");
 
   free (ws);
 
