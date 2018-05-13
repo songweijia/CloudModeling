2596cf8d8744e46299554d41b42e38ed10c77b42
Weijia <songweijia@gmail.com>
2018-05-13 16:53:16 +0000

    added kmeans

--- benchmark/fastcloudmodel/data/buffer_size/profile.sh
+++ benchmark/fastcloudmodel/data/buffer_size/profile.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+KMDAT=kmeans.dat
 
+cd kmeans
+g++ -o kmeans kmeans.cpp
+cd ..
+
+NDP=`cat bs | wc -l`
+echo $NDP 1 4 100 1 > ${KMDAT}
 for bs in `cat bs`
 do
-  sudo ../../cm -e throughput -s ${bs} -n 32 -S 128 | grep WRITE | awk -v bs=$bs '{print bs","$2","$5}'
+  sudo ../../cm -e throughput -s ${bs} -n 32 -S 128 | grep WRITE | awk -v bs=$bs '{print $2" "bs}' >> ${KMDAT}
 done
+
+cat ${KMDAT} | kmeans/kmeans | grep ^Cluster\ values | awk '{print $3}' | sort -k 1 -n > profile.res
