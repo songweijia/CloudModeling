7fb22258f1d883a8596c398af95bc28164467201
Weijia <ws393@cornell.edu>
2018-07-06 00:24:35 -0400

    new profile tools

--- benchmark/fastcloudmodel/profile/kde.py
+++ benchmark/fastcloudmodel/profile/kde.py
@@ -6,19 +6,20 @@ import sys
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-  if len(sys.argv) < 2:
-    print "%s <data file> [bin=0.05]" % sys.argv[0]
+  if len(sys.argv) < 3:
+    print "%s <data file> <cache level> [bin=0.1]" % sys.argv[0]
     exit(0)
   dat = np.loadtxt(sys.argv[1])
-  binsize = 0.05
-  if len(sys.argv) >= 3:
-    binsize = float(sys.argv[2])
+  cl = int(sys.argv[2])
+  binsize = 0.1
+  if len(sys.argv) >= 4:
+    binsize = float(sys.argv[3])
   kernel = gaussian_kde(dat,bw_method=binsize)
   x = np.arange(0,max(dat)+1,0.01)
   density = kernel(x)
   plt.plot(x,density)
   plt.show()
   maxima_x, = argrelextrema(density, np.greater)
-  output = [x[i] for i in sorted(maxima_x, key=lambda(x):density[x],reverse=True)[0:4] ]
+  output = [x[i] for i in sorted(maxima_x, key=lambda(x):density[x],reverse=True)[0:(cl+1)] ]
   for thp in sorted(output,reverse=True):
     print "%.3f" % thp
--- benchmark/fastcloudmodel/profile/profile.sh
+++ benchmark/fastcloudmodel/profile/profile.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
+if [ $# -gt 0 ]; then
+  CORE_ID=$1
+else
+  CORE_ID=0
+fi
+
 OUTPUT=raw.dat
 THPDAT=thp.dat
 
+CSH=(`lscpu | grep cache | sed '/L1i/d' | awk '{print $3}' | sed 's/K//'`)
+
+# step 1 throughput
 rm -rf ${OUTPUT}
 
 NDP=`cat bs | wc -l`
 for bs in `cat bs`
 do
-  sudo taskset 0x2 ../cm -e throughput -s ${bs} -n 32 -S 128 | grep WRITE | awk -v bs=$bs '{print $9" "bs}' >> ${OUTPUT}
+  sudo taskset -c ${CORE_ID} ../cm -e throughput -s ${bs} -n 32 -S 128 | grep WRITE | awk -v bs=$bs '{print $9" "bs}' >> ${OUTPUT}
 done
 
 cat ${OUTPUT} | awk '{print $1}' >> ${THPDAT}
 
 # cat ${KMDAT} | kmeans/kmeans | grep ^Cluster\ values | awk '{print $3}' | sort -k 1 -n > profile.res
-./kde.py ${THPDAT} >> profile.res
+./kde.py ${THPDAT} ${#CSH[@]} > profile.thp
 
 rm -rf ${THPDAT}
+
+# step 2 find cache size
+CS=(`cat profile.thp`)
+
+for((i=0;i<${#CSH[@]};i++))
+do
+  let l=$i+1
+  buffer_size=`sudo taskset -c ${CORE_ID} ../cm -e cachesize -S 128 -u ${CS[$i]} -l ${CS[$l]} -c ${CSH[$i]} -n 20 -N 10 | head -1 | awk '{print $2}' | sed 's/KB//'`
+  #test_buffer_size=`expr ${buffer_size} \* 9 \/ 10`
+  test_buffer_size=$buffer_size
+  sudo taskset -c ${CORE_ID} ./rr_lat ${test_buffer_size} 20 | awk '{SUM+=$3} END {print SUM / NR " ns"}' >> profile.lat
+done
+
+buffer_size=`sudo taskset -c ${CORE_ID} ../cm -e cachesize -S 128 -u ${CS[-2]} -l ${CS[-1]} -c ${CSH[-1]} -n 20 -N 10 | tail -1 | awk '{print $2}' | sed 's/KB//'`
+test_buffer_size=`expr ${buffer_size} \* 10`
+sudo taskset -c ${CORE_ID} ./rr_lat ${test_buffer_size} 20 | awk '{SUM+=$3} END {print SUM / NR " ns"}' >> profile.lat
